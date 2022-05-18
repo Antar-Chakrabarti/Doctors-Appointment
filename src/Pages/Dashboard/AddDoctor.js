3b594738000fde2c0b06ca101c2ffd69f63477cc
Antar-chakrabarti <chakrabartiantar@gmail.com>
2022-05-18 11:38:15 +0600

    new updated

---
+++ src/Pages/Dashboard/AddDoctor.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
+import Loading from '../Shared/Loading';
+
+const AddDoctor = () => {
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+    const {data: services, isLoading} = useQuery('services', ()=>fetch('http://localhost:5000/service').then(res => res.json()));
+    const imgStorageKey = 'b2d0a6df95ccee38c72cf82329e0fc8a';
+    /**
+     * 3 ways to store images
+     * 1. third party storage // Free open public storage is ok for practice project
+     * 2. Your won storage in your own server(file system)
+     * 3. Database: Mongodb
+     * 
+     * YUP: to validate file: Search: Yup file validation for react hook form
+    */
+    const onSubmit = async data => {
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?key=${imgStorageKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => res.json())
+        .then(result => {
+            if(result.success){
+                const img = result.data.url;
+                const doctor = {
+                    name: data.name,
+                    email: data.email,
+                    specialty: data.specialty,
+                    img: img
+                }
+                // send to your database
+                fetch('http://localhost:5000/doctor',{
+                    method: 'POST',
+                    headers: {
+                        'content-type':'application/json',
+                        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                    },
+                    body: JSON.stringify(doctor)
+                })
+                .then(res => res.json())
+                .then(inserted => {
+                    console.log(inserted)
+                    if(inserted.insertedId){
+                        toast.success('Doctor added successfully');
+                        reset();
+                    }
+                    else{
+                        toast.error('Failed to add the doctor')
+                    }
+                })
+            }
+        })
+    };
+    if(isLoading){
+        return <Loading/>
+    }
+    return (
+        <div>
+            <h2 className="text-2xl">Add a New Doctor</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input
+                        {...register("name", {
+                            required: {
+                                value: true,
+                                message: 'Name is Required'
+                            }
+                        })}
+                        type="text"
+                        placeholder="Your Name"
+                        className="input input-bordered w-full max-w-xs" />
+                    <label className="label">
+                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                    </label>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input
+                        {...register("email", {
+                            required: {
+                                value: true,
+                                message: 'Email is Required'
+                            },
+                            pattern: {
+                                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                message: 'Provide a valid Email'
+                            }
+                        })}
+                        type="email"
+                        placeholder="Your Email"
+                        className="input input-bordered w-full max-w-xs" />
+                    <label className="label">
+                        {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                        {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                    </label>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Specialty</span>
+                    </label>
+                    <select {...register('specialty')} class="select w-full max-w-xs">
+                        <option selected>Pick your favorite Simpson</option>
+                        {
+                            services.map(service => <option
+                            key={service._id}
+                            value={service.name}
+                            >{service.name}</option>)
+                        }
+                    </select>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Photo</span>
+                    </label>
+                    <input
+                        {...register("image", {
+                            required: {
+                                value: true,
+                                message: 'Image is Required'
+                            }
+                        })}
+                        type="file"
+                        className="input input-bordered w-full max-w-xs" />
+                    <label className="label">
+                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                    </label>
+                </div>
+                <input className='btn w-full max-w-xs text-white' value='Add' type="submit" />
+            </form>
+        </div>
+    );
+};
+
+export default AddDoctor;
